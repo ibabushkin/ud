69b6f66c2f806b9cce2a39775d4c23594eaaf08e
lenoch <lenoch420@yandex.com>
2015-09-27 21:36:21 +0200

    urban dictionary init

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+extern crate rustc_serialize;
+extern crate hyper;
+
+use std::io::Read;
+use std::io::Error;
+use std::env;
+
+use hyper::Client;
+
+use rustc_serialize::json::Json;
+use rustc_serialize::json;
+
+fn get_definition(definition: &str) -> String{
+    let client = Client::new();
+    let base_url = "http://api.urbandictionary.com/v0/define?term=".to_string();
+    
+    let get_url = base_url + definition; 
+
+    let mut res = client.get(&get_url).send().unwrap();
+
+        
+    let mut json = String::new();
+    res.read_to_string(&mut json).unwrap();
+   
+    return json;
+}
+
+#[derive(RustcDecodable)]
+struct Definition{
+    author: String,
+    current_vote: String,
+    defid: i32,
+    definition: String,
+    example: String,
+    permalink: String,
+    thumbs_down: i32,
+    thumbs_up: i32,
+    word: String
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let definition = &args[1];
+        let data = get_definition(definition);
+        let json_data = Json::from_str(&data).unwrap();
+        let res = json_data.find_path(&["list"]).unwrap().to_string();
+        let definitions: Vec<Definition> = json::decode(&res).unwrap();
+
+        println!("{}", definitions[0].definition);
+    }else{
+        println!("Invalid number of arguments");
+    }
+
+}
+
+
